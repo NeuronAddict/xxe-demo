3b6fc69f92fd3975c3592bf3c455ebc688893398
neuronaddict <manufrommars@elacourarie.org>
2020-07-28 23:31:55 +0200

    Obfusced message option.

--- src/main/java/org/lacourarie/xxedemo/XxeController.java
+++ src/main/java/org/lacourarie/xxedemo/XxeController.java
@@ -1,12 +1,21 @@
 package org.lacourarie.xxedemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.xml.stream.XMLInputFactory;
+import java.util.logging.Logger;
+
 @RestController
 public class XxeController {
+
+    private static final Logger LOGGER = Logger.getLogger(XMLInputFactory.class.getCanonicalName());
+
+    @Value("#{new Boolean('${org.lacourarie.xxedemo.obfusced}')}")
+    private Boolean obfusced;
 
     @GetMapping
     public Message get() {
@@ -15,7 +24,12 @@ public class XxeController {
 
     @PostMapping()
     public Message post(@RequestBody Message message) {
-        return message;
+
+        LOGGER.info("a message is send..." + message);
+
+        // Send message ...
+
+        return new Message(String.format("Your message (%s) has been send", (this.obfusced) ? "###" : message.getText()));
     }
 
 }
