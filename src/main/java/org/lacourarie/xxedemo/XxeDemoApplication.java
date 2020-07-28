cc12f507a427688cf23ee175d0b111f13804b88f
neuronaddict <manufrommars@elacourarie.org>
2020-07-28 22:36:26 +0200

    Remove explicit XXE

--- src/main/java/org/lacourarie/xxedemo/XxeDemoApplication.java
+++ src/main/java/org/lacourarie/xxedemo/XxeDemoApplication.java
@@ -25,9 +25,9 @@ public class XxeDemoApplication {
     public XmlMapper xmlMapper() {
         XMLInputFactory xmlIn = XMLInputFactory.newInstance();
         // as per [dataformat-xml#190], disable external entity expansion by default
-        xmlIn.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, Boolean.TRUE);
-        xmlIn.setProperty(XMLInputFactory.SUPPORT_DTD, Boolean.TRUE);
-        xmlIn.setProperty(XMLInputFactory.IS_REPLACING_ENTITY_REFERENCES, Boolean.TRUE);
+        //xmlIn.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, Boolean.TRUE);
+        //xmlIn.setProperty(XMLInputFactory.SUPPORT_DTD, Boolean.TRUE);
+        //xmlIn.setProperty(XMLInputFactory.IS_REPLACING_ENTITY_REFERENCES, Boolean.TRUE);
         XMLOutputFactory xmlOut = XMLOutputFactory.newInstance();
         XmlFactory xf = new XmlFactory(xmlIn, xmlOut);
         return new XmlMapper(xf);
