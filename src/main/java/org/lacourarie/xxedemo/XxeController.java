922130947eccc849a96e2c3b6329ebed38d4a297
emmanuel.lacourarie-ext <emmanuel.lacourarie-ext@mousquetaires.com>
2020-07-28 18:07:28 +0200

    initial commit

---
+++ src/main/java/org/lacourarie/xxedemo/XxeController.java
@@ -0,0 +1,23 @@
+package org.lacourarie.xxedemo;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class XxeController {
+
+    @GetMapping
+    public Message get() {
+        return new Message("Hi! I'm a message, you can post me ;)");
+    }
+
+    @PostMapping()
+    public Message post(@RequestBody Message message) {
+        return message;
+    }
+
+}
---
+++ src/main/java/org/lacourarie/xxedemo/XxeDemoApplication.java
@@ -0,0 +1,41 @@
+package org.lacourarie.xxedemo;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.dataformat.xml.XmlFactory;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
+import com.sun.xml.internal.fastinfoset.stax.factory.StAXInputFactory;
+import com.sun.xml.internal.fastinfoset.stax.factory.StAXOutputFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLOutputFactory;
+
+@SpringBootApplication
+public class XxeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxeDemoApplication.class, args);
+    }
+
+
+    public XmlMapper xmlMapper() {
+        XMLInputFactory xmlIn = XMLInputFactory.newInstance();
+        // as per [dataformat-xml#190], disable external entity expansion by default
+        xmlIn.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, Boolean.TRUE);
+        xmlIn.setProperty(XMLInputFactory.SUPPORT_DTD, Boolean.TRUE);
+        xmlIn.setProperty(XMLInputFactory.IS_REPLACING_ENTITY_REFERENCES, Boolean.TRUE);
+        XMLOutputFactory xmlOut = XMLOutputFactory.newInstance();
+        XmlFactory xf = new XmlFactory(xmlIn, xmlOut);
+        return new XmlMapper(xf);
+    }
+
+    @Bean
+    public MappingJackson2XmlHttpMessageConverter  converter() {
+        return xmlMapper();
+    }
+
+}
