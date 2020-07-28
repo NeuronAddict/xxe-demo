f68dd7873ff0335866ff5fe31161751add61e1f6
neuronaddict <manufrommars@elacourarie.org>
2020-07-28 22:35:22 +0200

    Clean controller code.

--- src/main/java/org/lacourarie/xxedemo/XxeController.java
+++ src/main/java/org/lacourarie/xxedemo/XxeController.java
@@ -1,11 +1,9 @@
 package org.lacourarie.xxedemo;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class XxeController {
@@ -16,7 +14,7 @@ public class XxeController {
     }
 
     @PostMapping()
-    public Message post(@RequestBody Message message) throws JsonProcessingException {
+    public Message post(@RequestBody Message message) {
         return message;
     }
 
