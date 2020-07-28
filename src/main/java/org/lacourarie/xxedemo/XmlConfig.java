0037434a88a498bf02406284130dfef5d9f6df3a
neuronaddict <manufrommars@elacourarie.org>
2020-07-28 23:21:48 +0200

    add option on xml factory config.

--- src/main/java/org/lacourarie/xxedemo/XmlConfig.java
+++ src/main/java/org/lacourarie/xxedemo/XmlConfig.java
@@ -14,11 +14,8 @@ public class XmlConfig {
 
     public XmlMapper xmlMapper() {
         XMLInputFactory xmlIn = XMLInputFactory.newInstance();
-        // as per [dataformat-xml#190], disable external entity expansion by default
-        //xmlIn.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, Boolean.TRUE);
-        //xmlIn.setProperty(XMLInputFactory.SUPPORT_DTD, Boolean.TRUE);
-        //xmlIn.setProperty(XMLInputFactory.IS_REPLACING_ENTITY_REFERENCES, Boolean.TRUE);
         XMLOutputFactory xmlOut = XMLOutputFactory.newInstance();
+        xmlIn.setProperty(XMLInputFactory.IS_NAMESPACE_AWARE, Boolean.TRUE);
         XmlFactory xf = new XmlFactory(xmlIn, xmlOut);
         return new XmlMapper(xf);
     }
