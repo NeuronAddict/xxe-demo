b760c7c4c49504eae906ca6c1884afe1ce9f4210
neuronaddict <manufrommars@elacourarie.org>
2020-12-06 23:18:52 +0100

    toString in Message

--- src/main/java/org/lacourarie/xxedemo/Message.java
+++ src/main/java/org/lacourarie/xxedemo/Message.java
@@ -1,5 +1,7 @@
 package org.lacourarie.xxedemo;
 
+import java.util.Objects;
+
 public class Message {
 
     private String text;
@@ -18,5 +20,25 @@ public class Message {
 
     public void setText(String text) {
         this.text = text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message = (Message) o;
+        return Objects.equals(text, message.text);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(text);
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "text='" + text + '\'' +
+                '}';
     }
 }
