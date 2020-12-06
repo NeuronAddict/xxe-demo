39cc10a7b8507e790492ea112088177ad3373cda
neuronaddict <manufrommars@elacourarie.org>
2020-12-06 23:06:13 +0100

    Revert "Version with spring boot 1.3.8"
    This reverts commit 54872cfd

--- src/main/java/org/lacourarie/xxedemo/XxeController.java
+++ src/main/java/org/lacourarie/xxedemo/XxeController.java
@@ -1,27 +1,28 @@
 package org.lacourarie.xxedemo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.xml.stream.XMLInputFactory;
 import java.util.logging.Logger;
 
 @RestController
 public class XxeController {
 
-    private static final Logger LOGGER = Logger.getLogger(XxeController.class.getCanonicalName());
+    private static final Logger LOGGER = Logger.getLogger(XMLInputFactory.class.getCanonicalName());
 
     @Value("#{new Boolean('${org.lacourarie.xxedemo.obfuscated}')}")
     private Boolean obfuscated;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public Message get() {
         return new Message("Hi! I'm a message, you can post me ;)");
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping()
     public Message post(@RequestBody Message message) {
 
         LOGGER.info("a message is send..." + message);
